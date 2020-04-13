6ba8534a2f98f9a1ed955faae9439b2ff4b46dfd
ashishpolaki <61271327+ashishpolaki@users.noreply.github.com>
2020-04-13 19:17:10 +0530

    Added a project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public CharacterController playercontroller ;
+    private Collider collider;
+
+    private void Awake()
+    {
+        
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
