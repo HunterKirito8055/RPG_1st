101ddc4053b67fd63976eef070b5141eb1428714
ashishpolaki <61271327+ashishpolaki@users.noreply.github.com>
2020-04-14 17:19:51 +0530

    Created Player Movement and Animation part

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,95 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator P_anim;
+    private Camera maincamera;
+
+    private PlayerMovement player;
+    private Vector3 direction;
+
+    public float speed_attack = 0.05f;
+    public float speed_Whileattacking = 0.1f; 
+    public float speed = 0.7f;
+    public float turnspeed =50f;
+    public float jumpspeed = 20f;
+    public float move_magnitude = 0.05f;
+
+    private float speed_movemultiplier = 1f;
+
+    private void Awake()
+    {
+        P_anim = GetComponent<Animator>();
+        player = GetComponent<PlayerMovement>();
+    }
     void Start()
     {
-        
+        P_anim.applyRootMotion = false;
+        maincamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movementandjumping();
+    }
+    private Vector3 MoveDirection
+    {
+        get { return direction;  }
+        set
+        {
+            direction = value * speed_movemultiplier;
+            if(direction.magnitude > 0.1f)
+            {
+                var newRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * turnspeed);
+            }
+            direction *= speed *(Vector3.Dot(transform.forward,direction ) + 1f)  * 5f;
+            player.move(direction);
+            animationmove(player.playercontroller.velocity.magnitude * 0.1f);
+        }
+    }
+    void moving(Vector3 dir, float mult)
+    {
+        speed_movemultiplier = mult * 1f;
+        MoveDirection = dir;
+    }
+    void animationmove(float magnitude)
+    {
+        if (magnitude > move_magnitude)
+        {
+            float Speed_animation = 2f * magnitude;
+
+            if (Speed_animation < 1f)
+                Speed_animation = 1f;
+            if (P_anim.GetInteger("State") != 2)
+            {
+                P_anim.SetInteger("State", 1);
+                P_anim.speed = Speed_animation;
+            }
+
+        }
+        else if (P_anim.GetInteger("State") != 2)
+        {
+            P_anim.SetInteger("State", 0);
+
+        }
+    }
+    void jumping()
+    {
+        player.jump(jumpspeed);
+    }
+    void Movementandjumping()
+    {
+        Vector3 moveinput = Vector3.zero;
+        Vector3 forward = Quaternion.AngleAxis(-90f, Vector3.up) * maincamera.transform.right;
+
+        moveinput += forward * Input.GetAxis("Vertical");
+        moveinput += maincamera.transform.right * Input.GetAxis("Horizontal");
+
+        moveinput.Normalize();
+        moving(moveinput.normalized, 1f);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jumping();
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,73 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController playercontroller ;
     private Collider collider;
 
+    public float Lerptime = 10f;
+    public float Gravity_multiplier = 1f;
+    public float Distance_to_Ground = 0.1f;
+    private float Fall_velocity = 0f;
+
+    private bool IsGrounded;
+
+    private Vector3 MoveDirection = Vector3.zero;
+    private Vector3 TargetDirection = Vector3.zero;
+
     private void Awake()
     {
-        
+        collider = GetComponent<Collider>();
+        playercontroller = GetComponent<CharacterController>();
     }
 
 
 
     void Start()
     {
-        
+        Distance_to_Ground = collider.bounds.extents.y; // half size of the collider bounds
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IsGrounded = OnGroundCheck();
+        MoveDirection = Vector3.Lerp(MoveDirection, TargetDirection, Time.deltaTime * Lerptime);
+        MoveDirection.y = Fall_velocity; //its for bring back our plyr to ground
+
+        playercontroller.Move(MoveDirection * Time.deltaTime);
+        if (!IsGrounded)
+        {
+            Fall_velocity -= Gravity_multiplier * 90f * Time.smoothDeltaTime;
+        }
+    }
+
+    //checking player whether he is onGround 
+    private bool OnGroundCheck()
+    {
+        RaycastHit hit;
+        if (playercontroller.isGrounded)
+        {
+            return true;
+        }
+        if(Physics.Raycast(collider.bounds.center,-Vector3.up,out hit,Distance_to_Ground + 0.1f))
+        {
+            return true;
+        }
+        return false;
+    }
+    public void move(Vector3 dir)
+    {
+        TargetDirection = dir;
+    }
+    public void stop()
+    {
+        MoveDirection = Vector3.zero;
+        TargetDirection = Vector3.zero;
+    }
+    public void jump(float jumpspeed)
+    {
+        if (IsGrounded)   //if we r on Ground then fallvelocity equals to jumpspeed to make our player jump
+        {
+            Fall_velocity = jumpspeed;
+        }
+
     }
 }
